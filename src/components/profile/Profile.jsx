d219e2524f28047b82ec192b007fdb831fbdbdb4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-28 18:33:11 +0300

    Update Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import s from './Profile.module.css'
-import Post from "./MyPosts/Posts/Post";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPosts from "./MyPosts/MyPosts";
-const Profile = ()=> {
-    return <div className={s.content}>
+
+const Profile = () => {
+    return <div>
         <div>
-            <img src='https://s0.rbk.ru/v6_top_pics/media/img/1/16/755904759776161.jpg'/>
-        </div>
-        <div>
-            ava + discr
+            <ProfileInfo/>
         </div>
         <div>
             <MyPosts/>
         </div>
-        </div>
+    </div>
 }
 
 

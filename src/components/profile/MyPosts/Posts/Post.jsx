f1e14a67c02472837f4543589e150f4e962d2957
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-28 18:33:06 +0300

    Update Post.jsx

--- src/components/profile/MyPosts/Posts/Post.jsx
+++ src/components/profile/MyPosts/Posts/Post.jsx
@@ -3,9 +3,11 @@ import s from './Post.module.css'
 
 const Post = (props) => {
     return (
-        <div className={s.item}>
-            <img src='https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'/>
-            {props.message}
+        <div>
+            <div className={s.item}>
+                <img src='https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'/>
+                {props.message}
+            </div>
             <div>
                 <span>Likes</span> {props.likescounts}
             </div>

af79ab0ad29d4a7e2305892d3348673e19682cf1
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-28 18:32:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/header/header";
 import Nav from "./components/nav/nav";
-import Profile from "./components/profile/Profile";
+import ProfileInfo from "./components/profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
-const App = () => {
+const App = (props) => {
     return (
-        <div className='App-wrapper'>
-            <Header/>
-            <Nav/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className='App-wrapper'>
+                <Header/>
+                <Nav/>
+                <div className='App-wrapper-content'>
+                    <Routes>
+                        <Route path='/profile' element={<ProfileInfo/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 

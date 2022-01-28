4f8df25639db199a80242f27572d5a992ca5891c
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-28 18:32:31 +0300

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import s from './header.module.css';
 const Header = () => {
     return <header className={s.header}>
         <img
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png'/>
+            src='https://i.gifer.com/UOkf.gif'/>
     </header>
 }
 

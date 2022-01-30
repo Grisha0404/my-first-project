7c3a67e1380a36f2b89277ee26e9a6cb3e2b2fcd
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-30 17:06:44 +0300

    s

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,20 @@ import Post from "./Posts/Post";
 
 const MyPosts = (props) => {
     let postsElement = props.posts.map(p => <Post message={p.message} likesCounts={p.likesCounts}/>);
+    let newPostElement = React.createRef();
+    let addPosts = () => {
+        let text = newPostElement.current.value;
+        props.addPosts(text)
+    }
     return (
         <div className={s.MyPost}>
             My post
             <div className={s.textBlock}>
                 <div>
-                    <textarea> </textarea>
+                    <textarea ref={newPostElement}> </textarea>
                 </div>
-                <div>
-                    <button>Add post</button>
+                <div className={s.button}>
+                    <button onClick={addPosts}>Add post</button>
                 </div>
             </div>
             <div>

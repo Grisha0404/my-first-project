fedcc8f33c5e2847a73bb2b9f59c450bef49b554
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-30 17:07:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,28 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from "./redux/state";
+import {addPosts} from "./redux/state";
 
-let dialogs = [
-    {id: 1, name: 'Pasha'},
-    {id: 2, name: 'Olya'},
-    {id: 3, name: 'Sasha'},
-    {id: 4, name: 'Leha'}
-];
-
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How are you?'},
-    {id: 3, message: 'Yo'},
-    {id: 4, message: 'Yo'}
-];
-
-let posts = [
-    {id: 1, message: 'Hi, how are you?', likesCounts: 11},
-    {id: 2, message: "It's my first post!", likesCounts: 25}
-];
 ReactDOM.render(
     <React.StrictMode>
-        <App posts={posts} dialogs={dialogs} messages={messages}/>
+        <App state={state} addPosts={addPosts}/>
     </React.StrictMode>,
     document.getElementById('root')
 );

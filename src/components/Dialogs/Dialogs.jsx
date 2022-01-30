ca296df0f63c009927965fe955cdb9b99ac532d8
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-30 11:55:46 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,37 +1,11 @@
 import React from 'react'
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
-
-const DialogInfo = (props) => {
-    let path = '/dialogs/' + props.id;
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
-
-const Message = (props) => {
-    return <div className={s.message}>
-        {props.message}
-    </div>
-}
-
-let dialogs = [
-    {id: 1, name: 'Pasha'},
-    {id: 2, name: 'Olya'},
-    {id: 3, name: 'Sasha'},
-    {id: 4, name: 'Leha'}
-]
-let dialogsElement = dialogs.map(d => <DialogInfo name={d.name} id={d.id}/>);
-
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How are you?'},
-    {id: 3, message: 'Yo'},
-    {id: 4, message: 'Yo'}
-]
-let messagesElements = messages.map(m => <Message message={m.message}/>);
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Massege/Message";
 
 const Dialogs = (props) => {
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
+    let dialogsElement = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>

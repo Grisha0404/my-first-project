6c87eb1a18ad0035ab2a6dadd5cef32e295e77dd
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-01-30 11:56:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let dialogs = [
+    {id: 1, name: 'Pasha'},
+    {id: 2, name: 'Olya'},
+    {id: 3, name: 'Sasha'},
+    {id: 4, name: 'Leha'}
+];
+
+let messages = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you?'},
+    {id: 3, message: 'Yo'},
+    {id: 4, message: 'Yo'}
+];
+
+let posts = [
+    {id: 1, message: 'Hi, how are you?', likesCounts: 11},
+    {id: 2, message: "It's my first post!", likesCounts: 25}
+];
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App posts={posts} dialogs={dialogs} messages={messages}/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+

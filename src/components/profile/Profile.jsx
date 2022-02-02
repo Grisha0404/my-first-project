6b944bb9e53face147824ed36a43eace2e3554be
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-02 17:53:10 +0300

    Update Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPosts from "./MyPosts/MyPosts";
+import {updateNewPostText} from "../../redux/state";
 
 const Profile = (props) => {
     return <div>
@@ -8,7 +9,10 @@ const Profile = (props) => {
             <ProfileInfo/>
         </div>
         <div>
-            <MyPosts posts={props.posts} addPosts={props.addPosts}/>
+            <MyPosts posts={props.profilePage.posts}
+                     newPostText={props.profilePage.newPostText}
+                     updateNewPostText={props.updateNewPostText}
+                     addPosts={props.addPosts}/>
         </div>
     </div>
 }

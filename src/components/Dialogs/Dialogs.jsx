e607affd5b878daf9a3aed6611d1ac8635f86b3e
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-03 11:06:50 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import s from './Dialogs.module.css'
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Massege/Message";
+import DialogItem, {dialogItemType, dialogType} from "./DialogItem/DialogItem";
+import Message, {messageType} from "./Massege/Message";
 
+type dialogsPropsType = {
+    messages: Array<messageType>,
+    dialogs: Array<dialogItemType>,
+    addMessage:string
+}
 
-const Dialogs = (props) => {
+const Dialogs = (props: dialogsPropsType) => {
     let messagesElements = props.messages.map(m => <Message message={m.message}/>);
     let dialogsElement = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} ava={d.ava}/>);
     let newMessageElement = React.createRef();
